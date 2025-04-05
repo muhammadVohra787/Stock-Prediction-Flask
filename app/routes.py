50bae6189ff079dc74d0a9b87a4937b628dba866
hhub25 <hetvi853@gmail.com>
2025-04-04 21:14:17 -0400

    Added these functionalities - login, logout, landing page, buy, sell, holdings, navbar - everything tested - UI needs to be improved

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,19 @@
 # app/routes.py
 from flask import Blueprint
-from app.controllers.user_controller import home, add_user, get_users, stock_selected
+from app.controllers.user_controller import home, add_user, get_users, stock_selected, buy_stock, sell_stock, get_holdings, login_user, logout_user, home_page
 
 main = Blueprint('main', __name__)
 
 # Associate the controller functions with the routes
-main.add_url_rule('/', 'home', home)
-main.add_url_rule('/register', 'register', add_user, methods=['POST'])
+# main.add_url_rule('/', 'home', home)
+# New default home route (public landing page)
+main.add_url_rule('/', 'home_page', home_page)
+main.add_url_rule('/dashboard', 'home', home)  
+main.add_url_rule('/register', 'register', add_user, methods=['GET', 'POST'])
 main.add_url_rule('/get_users', 'get_users', get_users, methods=['GET'])
 main.add_url_rule('/stock_selected', 'stock_selected' ,stock_selected,methods=['POST'])
+main.add_url_rule('/buy_stock', 'buy_stock', buy_stock, methods=['POST'])
+main.add_url_rule('/sell_stock', 'sell_stock', sell_stock, methods=['POST'])
+main.add_url_rule('/user_holdings/<user_id>', 'user_holdings', get_holdings, methods=['GET'])
+main.add_url_rule('/login', 'login', login_user, methods=['GET', 'POST'])
+main.add_url_rule('/logout', 'logout', logout_user)
