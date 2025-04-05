0c1c7f9c79dc5950e617d264e5f408a69f6b4f11
muhammadVohra787 <mvohra10@my.centennialcollege.ca>
2025-04-05 10:35:27 -0400

    added live reloading

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
 from app import create_app
+from livereload import Server
+import os
 
 app = create_app()
+app.debug = True
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    server = Server(app.wsgi_app)
+    
+    print("Current working directory:", os.getcwd())
+    print("Files in templates directory:", os.listdir('./app/templates/'))
+
+    server.watch('./app/templates/**/*.html')
+    server.watch('./app/static/**/*')
+    server.watch('app/**/*.py')
+
+    server.serve(port=5000, host='127.0.0.1')  # You can also set open_url_delay=True
